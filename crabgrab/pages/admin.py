7d45b633f343c501deeb8a0a03d87ac8b73993e3
LechX <lech.kaiel@gmail.com>
2017-03-17 17:23:21 -0700

    updated admin

--- crabgrab/pages/admin.py
+++ crabgrab/pages/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Tides
+from .models import Tides, Locations
 
 # Register your models here.
 admin.site.register(Tides)
+admin.site.register(Locations)
